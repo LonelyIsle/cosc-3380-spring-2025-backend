27bc5125e0bde46b7afe48468a8e4485b8c2e1d4
unknown <Johnmuiruri352@gmail.com>
2025-02-10 18:19:34 -0600

    only node.js removed the express features

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const http = require('http');
 
-const app = express();
-app.use(cors());
-
-app.get('/', (re, res) => {
-    return res.json("Johnbosco it's working");
+const server = http.createServer((req, res) => {
+    res.end('Hello World!');
 })
 
-app.listen(8081, () =>{
-    console.log("listening")
+
+server.listen('3000', () =>{
+    console.log('It is working')
 })
+
+
+
