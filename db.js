c5018bbd81c5dba3e356b9119488acb26039d90d
William <115291083+LonelyIsle@users.noreply.github.com>
2025-03-20 23:57:59 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,14 @@
-import mysql from "mysql2";
-
-const useSSL = process.env.DB_SSL && typeof process.env.DB_SSL === "string" 
-    ? process.env.DB_SSL.toLowerCase() === "true" 
-    : false;
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    ssl: useSSL ? { rejectUnauthorized: false } : false
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to Azure MySQL!");
-    connection.release();
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
