5c49be6be33674887d728181cf58377dabb6ca32
longt2407 <longt2407@gmail.com>
2025-03-22 01:05:44 -0500

    add data init script

---
+++ scripts/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `employee` (`first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) VALUES ('Chad', 'Gilmore', 1, 30.00, 'manager@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'); -- 123456
+INSERT INTO `employee` (`first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) VALUES ('Peter', 'Parker', 0, 15.00, 'staff@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'); -- 123456
+INSERT INTO `config` (`key`, `value`) VALUES ('SUBSCRIPTION_VALUE', 0.2);
+INSERT INTO `config` (`key`, `value`) VALUES ('SHIPPING_FEE', 10.00);
+INSERT INTO `config` (`key`, `value`) VALUES ('SALE_TAX', 0.08);
