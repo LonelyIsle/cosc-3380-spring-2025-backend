dcd115eb8c7028fd9803f17b0b37d55790139b69
longt2407 <longt2407@gmail.com>
2025-03-21 23:39:08 -0500

    minor fix after merging

--- helpers/url.js
+++ helpers/url.js
@@ -1,39 +1,20 @@
-function _objectAssign(keys, ...objs) {
-    return objs.reduce((acc, curr) => {
-        for (let key of keys) {
-            if (curr[key] !== undefined && curr[key] !== "") {
-                acc[key] = curr[key];
-            }
-        }
-        return acc;
-    });
-}
-function objectAssign(keys, ...objs) {
-    let nullObj = {};
-    for (let key of keys) {
-        nullObj[key] = null;
+import { URL } from "url"
+
+function getURLObj(url) {
+    if (!URL.canParse(url)) {
+        url = `http://fakehost${url}`;
     }
-    return _objectAssign(keys, nullObj, ...objs);
+    return URL.parse(url);
 }
 
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function parseStr(val) {
-    try {
-        let temp = JSON.parse(val);
-        if (typeof temp === "string") {
-            temp = decodeURI(temp).trim();
-        }
-        return [temp, null];
-    } catch(e) {
-        return [val, e];
+function getPathnameObj(pathname) {
+    if (!URL.canParse(pathname)) {
+        pathname = `http://fakehost${pathname}`;
     }
+    return URL.parse(pathname);
 }
 
 export default {
-    objectAssign,
-    timeout,
-    parseStr
+    getURLObj,
+    getPathnameObj,
 }
