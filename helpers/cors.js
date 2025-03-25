53fcb7a954e555ad86b75cc4cd9bf32c6b60287f
longt2407 <longt2407@gmail.com>
2025-03-24 22:31:49 -0500

    fix cors

--- helpers/cors.js
+++ helpers/cors.js
@@ -1,10 +1,11 @@
 function cors(req, res, next) {
     try {
+        console.log("CORS");
         let method = req.method.toUpperCase();
         if (method === 'OPTIONS') {
             res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, PUT, PATCH, POST, DELETE");
-            res.setHeader("Access-Control-Request-Headers", "content-type")
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept")
             res.statusCode = 204;
             res.setHeader('Content-Length', '0');
             res.end();
