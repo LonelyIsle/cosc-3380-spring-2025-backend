60855671fbc44bb2c8d72880aff5d61e680afc1c
longt2407 <longt2407@gmail.com>
2025-04-04 22:46:12 -0500

    add sale_event to data.sql

--- scripts/data.sql
+++ scripts/data.sql
@@ -1,22 +1,34 @@
 -- EMPLOYEE --
-INSERT INTO `employee` (`first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) VALUES ('Chad', 'Gilmore', 1, 30.00, 'manager@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'); -- 123456
-INSERT INTO `employee` (`first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) VALUES ('Peter', 'Parker', 0, 15.00, 'staff@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'); -- 123456
-INSERT INTO `employee` (`first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) VALUES ('Aria', 'Park', 1, 30.00, 'employee@example.com', '$2b$10$o8xtDsSqut4PMZx7HLBgP.TcgNokt1J/K08X75Iu9SyCfMF3GIGbW'); -- Password123
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `role`, `hourly_rate`, `email`, `password`) 
+VALUES 
+(1, 'Chad', 'Gilmore', 1, 30.00, 'manager@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'), -- 123456
+(2, 'Peter', 'Parker', 0, 15.00, 'staff@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.'), -- 123456
+(3, 'Aria', 'Park', 1, 30.00, 'employee@example.com', '$2b$10$o8xtDsSqut4PMZx7HLBgP.TcgNokt1J/K08X75Iu9SyCfMF3GIGbW'); -- Password123
 
 -- CUSTOMER -- 
-INSERT INTO `customer` (`first_name`, `last_name`, `email`, `password`, `reset_password_question`, `reset_password_answer`) VALUES ('John', 'Cena', 'customer@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.', 'How many states are there in the United States?', '$2b$10$cfd3GSy13NfXlRh8fXZequ464lS21Q.Ec2EcahsOpX0y.jOZOV5jK'); -- 123456, 50
-INSERT INTO `customer` (`first_name`, `last_name`, `email`, `password`, `reset_password_question`, `reset_password_answer`) VALUES ('Ava', 'Dean', 'somerandom@gmail.com', '$2b$10$nD53mm4ndgrdeMwpLkMsbOwlXVIKY34/nxSb1swYO.Udt3/Pcl9fm', 'How many states are there in the United States?', '$2b$10$cfd3GSy13NfXlRh8fXZequ464lS21Q.Ec2EcahsOpX0y.jOZOV5jK'); -- IWant2Login!, 50
+INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`, `password`, `reset_password_question`, `reset_password_answer`) 
+VALUES 
+(1, 'John', 'Cena', 'customer@domain.com', '$2b$10$dqmrkVGpQxnlj/c.dv0OROiWGRicBoewqeyCiM1EQzKtFQlSIZKJ.', 'How many states are there in the United States?', '$2b$10$cfd3GSy13NfXlRh8fXZequ464lS21Q.Ec2EcahsOpX0y.jOZOV5jK'), -- 123456, 50
+(2, 'Ava', 'Dean', 'somerandom@gmail.com', '$2b$10$nD53mm4ndgrdeMwpLkMsbOwlXVIKY34/nxSb1swYO.Udt3/Pcl9fm', 'How many states are there in the United States?', '$2b$10$cfd3GSy13NfXlRh8fXZequ464lS21Q.Ec2EcahsOpX0y.jOZOV5jK'); -- IWant2Login!, 50
 
 -- CONFIG --
-INSERT INTO `config` (`key`, `value`) VALUES ('SUBSCRIPTION_VALUE', 0.2);
-INSERT INTO `config` (`key`, `value`) VALUES ('SHIPPING_FEE', 9.99);
-INSERT INTO `config` (`key`, `value`) VALUES ('SALE_TAX', 0.08);
+INSERT INTO `config` (`id`, `key`, `value`) 
+VALUES 
+(1, 'SUBSCRIPTION_VALUE', 0.2),
+(2, 'SHIPPING_FEE', 9.99),
+(3, 'SALE_TAX', 0.08);
 
 -- COUPON --
-INSERT INTO `coupon` (`code`, `value`, `start_at`, `end_at`, `type`, `description`)
+INSERT INTO `coupon` (`id`, `code`, `value`, `start_at`, `end_at`, `type`, `description`)
 VALUES
-('20PERCENT', 20.00, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 0, '20%'),
-('20BUCKS', 20.00, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 1, '20.00$');
+(1, '20PERCENT', 20.00, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 0, '20%'),
+(2, '20BUCKS', 20.00, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 1, '20.00$');
+
+-- SALE_EVENT --
+INSERT INTO `sale_event` (`id`, `coupon_id`, `start_at`, `end_at`, `title`, `description`)
+VALUES
+(1, 1, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 'Save 20% on Your Next Purchase!', 'Save 20% on Your Next Purchase!'),
+(2, 2, NOW(), DATE_ADD(NOW(), INTERVAL 31 DAY), 'Save 20$ on Your Next Purchase!', 'Save 20$ on Your Next Purchase!');
 
 -- CATEGORY --
 INSERT INTO `category` (`id`, `name`, `description`)
@@ -26,56 +38,56 @@ VALUES
 (3, 'Movies & TV Shows', 'Films and shows that appear on tv or theatres.');
 
 -- PRODUCT_CATEGORY --
-INSERT INTO `product_category` (`product_id`, `category_id`)
+INSERT INTO `product_category` (`id`, `product_id`, `category_id`)
 VALUES 
-(1,2),
-(2,2),
-(3,2),
-(4,1),
-(5,3),
-(6,2),
-(7,1),
-(8,2),
-(9,1),
-(10,1),
-(11,2),
-(12,1),
-(13,2),
-(14,1),
-(15,2),
-(16,1),
-(17,2),
-(18,1),
-(19,2),
-(20,1),
-(21,2),
-(22,1),
-(23,2),
-(24,1),
-(25,2),
-(26,1),
-(27,2),
-(28,1),
-(29,2),
-(30,1),
-(31,2),
-(32,1),
-(33,3),
-(34,1),
-(35,2),
-(36,1),
-(37,2),
-(38,1),
-(39,2),
-(40,1),
-(41,2),
-(42,1),
-(43,2),
-(44,1),
-(45,2),
-(46,1),
-(47,2),
-(48,1),
-(49,2),
-(50,1),
-(51,2);
+(1, 1, 2),
+(2, 2, 2),
+(3, 3, 2),
+(4, 4, 1),
+(5, 5, 3),
+(6, 6, 2),
+(7, 7, 1),
+(8, 8, 2),
+(9, 9, 1),
+(10, 10, 1),
+(11, 11, 2),
+(12, 12, 1),
+(13, 13, 2),
+(14, 14, 1),
+(15, 15, 2),
+(16, 16, 1),
+(17, 17, 2),
+(18, 18, 1),
+(19, 19, 2),
+(20, 20, 1),
+(21, 21, 2),
+(22, 22, 1),
+(23, 23, 2),
+(24, 24, 1),
+(25, 25, 2),
+(26, 26, 1),
+(27, 27, 2),
+(28, 28, 1),
+(29, 29, 2),
+(30, 30, 1),
+(31, 31, 2),
+(32, 32, 1),
+(33, 33, 3),
+(34, 34, 1),
+(35, 35, 2),
+(36, 36, 1),
+(37, 37, 2),
+(38, 38, 1),
+(39, 39, 2),
+(40, 40, 1),
+(41, 41, 2),
+(42, 42, 1),
+(43, 43, 2),
+(44, 44, 1),
+(45, 45, 2),
+(46, 46, 1),
+(47, 47, 2),
+(48, 48, 1),
+(49, 49, 2),
+(50, 50, 1),
+(51, 51, 2);
