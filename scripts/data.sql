ac6ae4002b10950f3d013f485b1207c58a876fc8
longt2407 <longt2407@gmail.com>
2025-04-04 22:22:12 -0500

    add category to data.sql

--- scripts/data.sql
+++ scripts/data.sql
@@ -11,3 +11,65 @@ INSERT INTO `customer` (`first_name`, `last_name`, `email`, `password`, `reset_p
 INSERT INTO `config` (`key`, `value`) VALUES ('SUBSCRIPTION_VALUE', 0.2);
 INSERT INTO `config` (`key`, `value`) VALUES ('SHIPPING_FEE', 9.99);
 INSERT INTO `config` (`key`, `value`) VALUES ('SALE_TAX', 0.08);
+
+-- CATEGORY --
+INSERT INTO `category` (`id`, `name`, `description`)
+VALUES
+(1, 'Animals', 'Teh things that walk among us.'),
+(2, 'Anime', 'Japanese Cartoons'),
+(3, 'Movies & TV Shows', 'Films and shows that appear on tv or theatres.');
+
+-- PRODUCT_CATEGORY --
+INSERT INTO `product_category` (`product_id`, `category_id`)
+VALUES 
+(1,2),
+(2,2),
+(3,2),
+(4,1),
+(5,3),
+(6,2),
+(7,1),
+(8,2),
+(9,1),
+(10,1),
+(11,2),
+(12,1),
+(13,2),
+(14,1),
+(15,2),
+(16,1),
+(17,2),
+(18,1),
+(19,2),
+(20,1),
+(21,2),
+(22,1),
+(23,2),
+(24,1),
+(25,2),
+(26,1),
+(27,2),
+(28,1),
+(29,2),
+(30,1),
+(31,2),
+(32,1),
+(33,3),
+(34,1),
+(35,2),
+(36,1),
+(37,2),
+(38,1),
+(39,2),
+(40,1),
+(41,2),
+(42,1),
+(43,2),
+(44,1),
+(45,2),
+(46,1),
+(47,2),
+(48,1),
+(49,2),
+(50,1),
+(51,2);
